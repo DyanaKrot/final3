8a22dfc09fbbfcdff88070f76a4d316258d38f9a
vlaleks <82763774+vlaleks@users.noreply.github.com>
2021-07-31 16:05:26 +1000

    cleaning2

--- public/sketch.js
+++ public/sketch.js
@@ -1,12 +1,7 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/ZjVyKXp9hec
-
 let list = document.getElementById("dashboard");
 
 const userName = prompt('Ваш ник');
-// Keep track of our socket connection
+
 var socket;
 var NumPlayer;
 var blob;
@@ -29,9 +24,6 @@ function drawshow(Players) {
 
 function setup() {
   createCanvas(1300, 800);
-  // Start a socket connection to the server
-  // Some day we would run this server somewhere else
-  // socket = io.connect('http://localhost:3000');
   socket = io();
   for ( var i=0; i<4000;i++){
     var x = random(-5*width,width*5)
@@ -39,7 +31,6 @@ function setup() {
     blobs[i] = new Blob(x,y,10);
 }
   blob = new Blob(random(width), random(height), StandartRadiusBlob);
-  // Make a little object with  and y
   var data = {
     x: blob.pos.x,
     y: blob.pos.y,
@@ -51,7 +42,6 @@ function setup() {
   socket.emit('start', data);
 
   socket.on('heartbeat', function(data) {
-    //console.log(data);
     Players = data;
   });
 }
@@ -60,12 +50,11 @@ function setup() {
 
 function draw() {
   background(160,160,160);
-  // console.log(blob.pos.x, blob.pos.y);
+
 
   translate(width / 2, height / 2);
   var newzoom = StandartRadiusBlob / blob.r;
   zoom = lerp(zoom, newzoom, 0.1);
-  // scale(zoom);
   translate(-blob.pos.x, -blob.pos.y);
 
   Players.sort((a, b) => a.r <= b.r ? 1 : -1);
@@ -88,7 +77,7 @@ function draw() {
         blobs.splice(i,1);
         blobs.push(new Blob(random(-width,width*2),random(-height,height*2),10)) // Спавн новой еды
     }
-    if ( blobs[i] != undefined ) // Иногда появлсяется ошибка
+    if ( blobs[i] != undefined ) 
     blobs[i].show();
   } 
 
