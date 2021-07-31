35b8c9432acb5e4f1cbca97bf59b3f41f6998f5b
vlaleks <82763774+vlaleks@users.noreply.github.com>
2021-07-31 16:03:52 +1000

    cleaning

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/ZjVyKXp9hec
-
-// Based off of Shawn Van Every's Live Web
-// http://itp.nyu.edu/~sve204/liveweb_fall2013/week3.html
-
 var Players = [];
-
-
 
 function Blob(id,name, x, y, r, mass,color) {
   this.id = id;
@@ -20,16 +10,12 @@ function Blob(id,name, x, y, r, mass,color) {
   this.color = color      ;
 }
 
-// Using express: http://expressjs.com/
-var express = require('express');
-// Create the app
-var app = express();
 
-// Set up the server
-// process.env.PORT is related to deploying on heroku
+var express = require('express');
+var app = express();
 var server = app.listen(process.env.PORT || 5000, listen);
 
-// This call back just tells us that the server has started
+
 function listen() {
   var host = server.address().address;
   var port = server.address().port;
@@ -38,8 +24,7 @@ function listen() {
 
 app.use(express.static('public'));
 
-// WebSocket Portion
-// WebSockets work with the HTTP server
+
 var io = require('socket.io')(server);
 
 
@@ -49,24 +34,20 @@ function heartbeat() {
   io.sockets.emit('heartbeat', Players);
 }
 
-// Register a callback function to run when we have an individual connection
-// This is run for each individual user that connects
+
 io.sockets.on(
   'connection',
-  // We are given a websocket object in our function
+ 
   function(socket) {
     console.log('We have a new client: ' + socket.id);
 
     socket.on('start', function(data) {
-      // console.log(data.name + ' ' + data.x + ' ' + data.y + ' ' + data.r);
       var blob = new Blob(socket.id, data.name, data.x, data.y, data.r, data.mass, data.color);
       console.log(data.mass)
       Players.push(blob);
-      // console.log(data)
     });
 
     socket.on('update', function(data) {
-      // console.log(socket.id + " " + data.x + " " + data.y + " " + data.r);
       var blob;
       for (var i = 0; i < Players.length; i++) {
         if (socket.id == Players[i].id) {
